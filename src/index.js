63dc1d4f9fef789a06e58d6356dba61ef4fcef3a
Steve Hobbs <steve@Steves-MacBook.local>
2018-10-01 21:31:43 +0100

    SVG image

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+import SVG from 'react-inlinesvg';
+
+<SVG
+    src="/images/steve-hobbs.svg"
+    preloader={<Loader />}
+    onLoad={(src) => {
+        myOnLoadHandler(src);
+    }}
+>
+  Here's some optional content for browsers that don't support XHR or inline
+  SVGs. You can use other React components here too. Here, I'll show you.
+  <img src="/images/steve-hobbs.png" />
+</SVG>
+
 
 import 'font-awesome/css/font-awesome.css';
 import './style.css';
 
-import Clock from './components/Timer/Clock';
-import Container from './Container';
+// import Clock from './components/Timer/Clock';
+// import Container from './Container';
 
-ReactDOM.render(<Container />, document.getElementById('requestRefresh'));
+ReactDOM.render(<Loader />, document.getElementById('steve-hobbs'));
 
-ReactDOM.render(<Clock />, document.getElementById('clock'));
+// ReactDOM.render(<Clock />, document.getElementById('clock'));
