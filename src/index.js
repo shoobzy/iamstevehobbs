a2876c4b8b3c17840f3264f0b0976b22c0df3b81
stevehobbsPB <steve@powder-blue.com>
2018-10-02 14:11:24 +0100

    Component setup

--- src/index.js
+++ src/index.js
@@ -1,11 +1,31 @@
-// import React from 'react'
-// // import { render } from 'react-dom'
-// import ReactSVG from 'react-svg'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ReactSVG from 'react-svg';
+import Graphic from './images/steve-hobbs.svg';
 
 import 'font-awesome/css/font-awesome.css';
 import './style.css';
 
-ReactDOM.render(<ReactSVG src="./images/steve-hobbs.svg" />, document.getElementById('steve-hobbs'));
+class Intro extends React.Component {
+  render() {
+    const style = {
+      margin: 20,
+      padding: 20,
+      color: 'black',
+      backgroundColor: this.props.color,
+    }
+
+    return (
+      <div style={style}>
+        <h1>Hi there, I am</h1>
+        {/* <h2>{process.env.NODE_ENV}</h2> */}
+        <ReactSVG src={Graphic} />
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(
+    <Intro />,
+    document.getElementById('steve-hobbs')
+);
