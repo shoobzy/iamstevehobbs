712c92aab2996ea29c0d2dba7a830e56357310c5
stevehobbsPB <steve@powder-blue.com>
2018-10-05 16:53:34 +0100

    Hero setup

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import React from 'react';
 //   BrowserRouter as Router,
 //   Link
 // } from 'react-router-dom';
+import { animateScroll as scroll, } from 'react-scroll';
+
 import ReactDOM from 'react-dom';
 import ReactSVG from 'react-svg';
 import Graphic from './images/steve__hobbs_inverted.svg';
@@ -10,16 +12,38 @@ import Plx from 'react-plx';
 
 import './style.css';
 
-const parallaxData = [
+(function () {
+  window.addEventListener('scroll', function () {
+    var intro = document.querySelector('.intro'),
+        hero = document.querySelector('.hero');
+
+    if (document.body.scrollTop || document.documentElement.scrollTop >= hero.scrollHeight) {
+      intro.classList.add('detached');
+    } else {
+      intro.classList.remove('detached');
+    }
+  });
+})();
+
+const parallaxScale = [
   {
     start: 0,
     end: 500,
     properties: [
       {
         startValue: 1,
-        endValue: 15,
+        endValue: 200,
         property: 'scale',
       },
+    ],
+  },
+];
+
+const parallaxFade = [
+  {
+    start: 0,
+    end: 250,
+    properties: [
       {
         startValue: 1,
         endValue: 0,
@@ -29,19 +53,87 @@ const parallaxData = [
   },
 ];
 
+class Scroller extends React.Component {
+  render() {
+    return (
+      <span onClick={() => scroll.scrollTo(
+        window.innerHeight, {
+          duration: 1200,
+          delay: 50,
+          smooth: 'easeInExpo',
+        }
+      )}
+      >Take a look</span>
+    )
+  }
+}
+
 class Intro extends React.Component {
   render() {
     return (
-      <Plx
-        className="hero__intro_graphic"
-        parallaxData={ parallaxData }
-      >
-      <ReactSVG
-        src={ Graphic }
-        className="wrapper-class-name"
-      />
-      </Plx>
-    )
+      <div className="intro">
+        <div className="intro__inner">
+          <p>kdfhgkfdhsgjkdfh kjdfhkjghdjk fg</p>
+          <p>kdfhgkfdhsgjkdfh kjdfhkjghdjk fg</p>
+          <p>kdfhgkfdhsgjkdfh kjdfhkjghdjk fg</p>
+          <p>kdfhgkfdhsgjkdfh kjdfhkjghdjk fg</p>
+          <p>kdfhgkfdhsgjkdfh kjdfhkjghdjk fg</p>
+          <p>kdfhgkfdhsgjkdfh kjdfhkjghdjk fg</p>
+          <p>kdfhgkfdhsgjkdfh kjdfhkjghdjk fg</p>
+          <p>kdfhgkfdhsgjkdfh kjdfhkjghdjk fg</p>
+          <p>kdfhgkfdhsgjkdfh kjdfhkjghdjk fg</p>
+          <p>kdfhgkfdhsgjkdfh kjdfhkjghdjk fg</p>
+          <p>kdfhgkfdhsgjkdfh kjdfhkjghdjk fg</p>
+          <p>kdfhgkfdhsgjkdfh kjdfhkjghdjk fg</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+class Hero extends React.Component {
+  render() {
+    const spanHeight = {
+      height: '100%',
+    }
+
+    return (
+      <div>
+        <div className="hero">
+          <Plx
+            parallaxData={ parallaxScale }
+            style= { spanHeight}
+          >
+            <Plx
+              parallaxData={ parallaxFade }
+              className="hero__inner"
+              style= { spanHeight}
+            >
+              <div className="hero__intro_fill"></div>
+              <div className="hero__intro_graphic">
+                <div className="hero__intro_fill"><h1>Hi there, I am</h1></div>
+                <ReactSVG
+                  src={ Graphic }
+                />
+                <div className="hero__arrow hero__intro_fill">
+                  <Scroller />
+                </div>
+              </div>
+              <div className="hero__intro_fill"></div>
+            </Plx>
+          </Plx>
+        </div>
+        <Intro />
+      </div>
+    );
+  }
+}
+
+class App extends React.Component {
+  render() {
+    return (
+        <Hero />
+    );
   }
 }
 
@@ -62,6 +154,6 @@ class Intro extends React.Component {
 // }
 
 ReactDOM.render(
-    <Intro />,
-    document.getElementById('hero__intro_graphic')
+  <App />,
+  document.getElementById('root')
 );
