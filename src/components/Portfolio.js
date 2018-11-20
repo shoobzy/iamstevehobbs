b8d4e060aa8f93b577bba0d8fbeac700582acb55
stevehobbsPB <steve@powder-blue.com>
2018-11-20 11:04:39 +0000

    Set up routing

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,8 +1,4 @@
 import React from 'react';
-// import {
-//   BrowserRouter as Router,
-//   Link
-// } from 'react-router-dom';
 
 import PortfolioItem from './Portfolio/PortfolioItem';
 
--- src/components/Portfolio/PortfolioItem.js
+++ src/components/Portfolio/PortfolioItem.js
@@ -1,4 +1,8 @@
 import React from 'react'
+import {
+  BrowserRouter as Router,
+  Link
+} from 'react-router-dom';
 
 class PortfolioItem extends React.Component {
   constructor(props) {
@@ -30,22 +34,23 @@ class PortfolioItem extends React.Component {
     } = this.props;
 
     return (
-      <div className="c-Portfolio--Item o-Grid--Item 1/2-TabletPortraitUp 1/3-TabletLandscapeUp">
-        <a
-          href={url}
-          onMouseEnter={this.onMouseEnter()}
-          onMouseLeave={this.onMouseLeave()}
-        >
-          <img className="c-Portfolio--ItemImg" src={img_url}/>
-          <div className="c-Portfolio--Content">
-            <div className="c-Portfolio--Text">
-              <h5 className="c-Portfolio--Title">{title}</h5>
-              <p className="c-Portfolio--Category">{category}</p>
+      <Router>
+        <div className="c-Portfolio--Item o-Grid--Item 1/2-TabletPortraitUp 1/3-TabletLandscapeUp">
+          <Link to={url}
+            onMouseEnter={this.onMouseEnter()}
+            onMouseLeave={this.onMouseLeave()}
+          >
+            <img className="c-Portfolio--ItemImg" src={img_url}/>
+            <div className="c-Portfolio--Content">
+              <div className="c-Portfolio--Text">
+                <h5 className="c-Portfolio--Title">{title}</h5>
+                <p className="c-Portfolio--Category">{category}</p>
+              </div>
             </div>
-          </div>
-          <div className="c-Portfolio--Background"></div>
-        </a>
-      </div>
+            <div className="c-Portfolio--Background"></div>
+          </Link>
+        </div>
+      </Router>
     )
   }
 
