6e1787b3dfaf1dc05129a89491c49df377f8a7c2
stevehobbsPB <steve@powder-blue.com>
2018-12-07 11:01:06 +0000

    Refactoring

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -4,13 +4,13 @@ class Intro extends React.Component {
   render() {
     return (
       <div className="c-Intro o-Grid">
-        <h1 className="c-Intro--Title o-Grid--Item 1/2-TabletPortraitUp">
+        <h1 className="c-Intro--Title o-Grid--Item 1/2-TabletPortraitUp-WithGutter">
           Digital<br></br>
           Designer<br></br>
           Front-End<br></br>
           Developer
         </h1>
-        <p className="c-Intro--Overview o-Grid--Item 1/2-TabletPortraitUp">With more than 8 years’ industry experience at combining web development and design.</p>
+        <p className="c-Intro--Overview o-Grid--Item 1/2-TabletPortraitUp-WithGutter">With more than 8 years’ industry experience at combining web development and design.</p>
       </div>
     )
   }
