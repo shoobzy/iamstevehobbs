5d95c7a9972d29f36691c565c749c146a8e9b7c6
stevehobbsPB <steve@powder-blue.com>
2019-01-24 12:33:41 +0000

    Removed unneeded component. Updated "class" to "className".

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ class Footer extends React.Component {
   render() {
     return (
       <div className="o-Footer o-Container">
-        <p className="c-Overview">Built with <a href="https://reactjs.org/" target="_blank"><i class="fab fa-react"></i></a></p>
+        <p className="c-Overview">Built with <a href="https://reactjs.org/" target="_blank"><i className="fab fa-react"></i></a></p>
         <p>Steve Hobbs &copy; {(new Date().getFullYear())}</p>
       </div>
     )
