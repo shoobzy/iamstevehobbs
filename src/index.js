d16f73000a89eb8e3c32c9803d23b8a126cf17b6
stevehobbsPB <steve@powder-blue.com>
2019-01-29 13:33:05 +0000

    Service Worker is automatically included

--- src/index.js
+++ src/index.js
@@ -6,14 +6,6 @@ import {
 } from "react-router-dom";
 import App from "./components/App";
 
-// Check that service workers are registered
-if ('serviceWorker' in navigator) {
-  // Use the window load event to keep the page load performant
-  window.addEventListener('load', () => {
-      navigator.serviceWorker.register('../dist/__/sw.js');
-  });
-}
-
 ReactDOM.render(
     <BrowserRouter>
       <Route path="/" component={App} />
