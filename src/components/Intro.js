e2939ee49cbcc6088e87df86caf3c863b3f8ace4
stevehobbsPB <steve@powder-blue.com>
2019-03-14 14:36:42 +0000

    Applied pose animation to intro

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,19 +1,23 @@
 import React from "react";
+import posed from "react-pose";
 
-class Intro extends React.Component {
-  render() {
-    return (
-      <div className="c-Intro o-Grid">
-        <h1 className="c-Intro--Title o-Grid--Item 1/2-TabletPortraitUp-WithGutter">
-          Digital<br></br>
-          Designer<br></br>
-          Front-End<br></br>
-          Developer
-        </h1>
-        <p className="c-Title--Delta o-Grid--Item 1/2-TabletPortraitUp-WithGutter">With more than 8 years’ industry experience at combining web development and design.</p>
-      </div>
-    )
-  }
+const IntroContainer = posed.div({
+  enter: { y: 0, opacity: 1 },
+  exit: { y: 50, opacity: 0 }
+});
+
+function Intro() {
+  return (
+    <IntroContainer className="c-Intro o-Grid">
+      <h1 className="c-Intro--Title o-Grid--Item 1/2-TabletPortraitUp-WithGutter">
+        Digital<br></br>
+        Designer<br></br>
+        Front-End<br></br>
+        Developer
+      </h1>
+      <p className="c-Title--Delta o-Grid--Item 1/2-TabletPortraitUp-WithGutter">With more than 8 years’ industry experience at combining web development and design.</p>
+    </IntroContainer>
+  )
 }
 
 export default Intro;
