0e5120dad777a5ff9d45b6c439eb1027548fb754
stevehobbsPB <steve@powder-blue.com>
2019-03-18 17:01:15 +0000

    Applied pose animation to footer icon

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,43 +1,40 @@
-import React from "react";
-import classnames from "classnames";
+import React, { useState } from "react";
+import posed from "react-pose";
 
-class Footer extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isHovered: false
-    };
-
-    this.handleHover = this.handleHover.bind(this);
+const ReactIcon = posed.i({
+  hoverable: true,
+  init: {
+    rotate: "359deg",
+  },
+  hover: {
+    rotate: "360deg",
+  },
+  transition: {
+    type: "spring",
+    stiffness: 800,
+    duration: 30000
   }
+});
 
-  handleHover() {
-    this.setState(prevState => ({
-      isHovered: !prevState.isHovered
-    }));
-  }
+function Footer() {
+  const [isHovered, setIsHovered] = useState(false);
+  const hover = () => setIsHovered(true);
+  const iconStyle = {color: "#61dafb"};
 
-  render() {
-    const iconStyle = {color: "#61dafb"};
-    const iconClass = this.state.isHovered ? "spin" : "";
-
-    return (
-      <div className="o-Footer o-Container">
-        <p className="c-Title--Charlie">
-          <a
-            href="https://reactjs.org/"
-            target="_blank"
-            style={iconStyle}
-            onMouseEnter={this.handleHover}
-            onMouseLeave={this.handleHover}
-          >
-            <i className={classnames("fab fa-react", iconClass)}></i>
-          </a>
-        </p>
-        <p>Steve Hobbs &copy; {(new Date().getFullYear())}</p>
-      </div>
-    )
-  }
+  return (
+    <div className="o-Footer o-Container">
+      <p className="c-Title--Charlie">
+        <a
+          href="https://reactjs.org/"
+          target="_blank"
+          style={iconStyle}
+        >
+          <ReactIcon className="fab fa-react"></ReactIcon>
+        </a>
+      </p>
+      <p>Steve Hobbs &copy; {(new Date().getFullYear())}</p>
+    </div>
+  )
 }
 
 export default Footer;
