ebd8da32f6efa86996247b1e68585cca54cb0a50
Steve Hobbs <steve@Steves-MacBook.local>
2019-05-24 10:25:23 +0100

    Removed loader from Moonshine project

--- src/components/Projects/Moonshine.js
+++ src/components/Projects/Moonshine.js
@@ -2,22 +2,19 @@ import React from "react";
 import Moonshine from "../../images/projects/moonshine/moonshine.webp";
 import MoonshineFallback from "../../images/projects/moonshine/moonshine.jpg";
 import MoonshineFull from "../../images/projects/moonshine/moonshine-full.png";
-import Loader from "../Loader";
 
 const PortfolioItem = React.lazy(() => import("../ProjectTpl"));
 
 function MoonshineProject() {
   return (
-    <React.Suspense fallback={<Loader/>}>
-      <PortfolioItem
-        title="Moonshine"
-        category="Digital, Print"
-        image_webp={Moonshine}
-        image_non_webp={MoonshineFallback}
-        overview="Inspired by the 1930's art style of Max Fleischer, this 'battle flag' was designed for a boat representing the name 'Moonshine'."
-        modal={MoonshineFull}
-      />
-    </React.Suspense>
+    <PortfolioItem
+      title="Moonshine"
+      category="Digital, Print"
+      image_webp={Moonshine}
+      image_non_webp={MoonshineFallback}
+      overview="Inspired by the 1930's art style of Max Fleischer, this 'battle flag' was designed for a boat representing the name 'Moonshine'."
+      modal={MoonshineFull}
+    />
   )
 }
 
