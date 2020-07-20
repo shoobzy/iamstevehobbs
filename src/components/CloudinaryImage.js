5caa33c37efec6e7de6c041e28842360779e2ec0
Steve Hobbs <obz1986@gmail.com>
2020-07-20 10:53:54 +0100

    Removed loader from Image component

--- src/components/CloudinaryImage.js
+++ src/components/CloudinaryImage.js
@@ -1,24 +1,21 @@
 import React, { Suspense } from "react";
-import Loader from "./Loader";
 
 const Img = React.lazy(() => import("react-cloudinary-lazy-image"));
 
 export default function CloudinaryImage(props) {
   return (
-    <Suspense fallback={<Loader/>}>
-      <Img
-        cloudName="iamstevehobbs-com"
-        imageName={props.src}
-        alt={props.alt}
-        fluid={{
-          maxWidth: props.maxWidth,
-          height: props.height
-        }}
-        className="c-Portfolio--ItemImg h-ResponsiveImg"
-        placeholderStyle={{
-          filter: "blur(20px)"
-        }}
-      />
-    </Suspense>
+    <Img
+      cloudName="iamstevehobbs-com"
+      imageName={props.src}
+      alt={props.alt}
+      fluid={{
+        maxWidth: props.maxWidth,
+        height: props.height
+      }}
+      className="c-Portfolio--ItemImg h-ResponsiveImg"
+      placeholderStyle={{
+        filter: "blur(20px)"
+      }}
+    />
   );
 };
