41c0c9e31e90f692c66cb40264e64f6bc881a217
Steve Hobbs <obz1986@gmail.com>
2020-07-24 20:31:38 +0100

    Refactoring

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,7 @@
-import React, { Suspense } from "react";
+import React from "react";
 import posed from "react-pose";
 import ScrollToTopOnMount from "./ScrollToTopOnMount";
-import Loader from "./Loader";
-
-const Portfolio = React.lazy(() => import("./Portfolio"));
+import Portfolio from "./Portfolio";
 
 const HomeContainer = posed.div({
   enter: { y: 0, opacity: 1, delay: 150 },
@@ -40,9 +38,7 @@ class Home extends React.Component {
             <p className="c-Title--Delta o-Grid--Item 1/2-TabletPortraitUp-WithGutter">With more than 8 years’ industry experience at combining web development and design.</p>
           </div>
         </div>
-        <Suspense fallback={<Loader/>}>
-          <Portfolio />
-        </Suspense>
+        <Portfolio />
       </HomeContainer>
     )
   }
